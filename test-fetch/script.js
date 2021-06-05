a8de57d1bd6eb7d5389f3b4eb0a4f07328b7db47
Robert <u1501851@rts-365.dk>
2021-06-05 17:52:39 +0200

    building a form for posting new animal

--- test-fetch/script.js
+++ test-fetch/script.js
@@ -1,8 +1,45 @@
 console.log('script loaded');
 
-(async function fetchAnimals() {
-    const req = await fetch("http://rowabo-myapi.herokuapp.com/api/v1/animals")
+// ==== FETCH ====
+const myApiCollection = ["animals", "foods", "accessories"]
+
+async function fetchMyApi(myApiCollection) {
+    const req = await fetch(`http://rowabo-myapi.herokuapp.com/api/v1/${myApiCollection}`)
     const res = await req.json()
+
     console.log(res);
-    document.querySelector('body').innerText = JSON.stringify(res)
-})()
+
+    print(res.result)
+}
+
+function print(data) {
+    const body =  document.querySelector('body')
+    
+    data.forEach(element => {
+        for ( const [key, value] of Object.entries(element)) {
+            body.innerHTML += `<p><span style="font-weight:bold">${key}</span> : ${value}</p> \n`     
+        }    
+    })
+}
+
+// fetchMyApi(myApiCollection[0])
+
+// ==== FORM ====
+const form = document.querySelector("#createAnimal");
+
+let submitAnimal = e => {
+    console.log("submit animal");
+    e.preventDefault()
+
+    const myFormData = new FormData();
+    myFormData.append("type", e.target.type.value)
+    myFormData.append("breed", e.target.breed.value)
+    myFormData.append("name", e.target.name.value)
+    myFormData.append("age", e.target.age.value)
+    myFormData.append("sex", e.target.sex.value)
+    myFormData.append("colors", e.target.colors.value)
+
+    console.log(myFormData);
+}
+
+form.addEventListener("submit", submitAnimal)
